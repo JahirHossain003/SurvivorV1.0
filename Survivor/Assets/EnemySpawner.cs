669907e9cf5e785cba58435e23a3d58fa9fe2f78
jahir <jahir.003@gmail.com>
2016-12-10 19:04:07 +0100

    commit

---
+++ Survivor/Assets/EnemySpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemy;
+	// Use this for initialization
+	void Start () {
+		GameObject tempEnemy = Instantiate(enemy,new Vector3(0f,0f,0f),Quaternion.identity) as GameObject;
+		tempEnemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
