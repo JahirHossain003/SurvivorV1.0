bbfa180c478083b5188b08856de7897c3aa904ae
jahir <jahir.003@gmail.com>
2016-12-10 23:28:25 +0100

    player projectile

---
+++ Survivor/Assets/Scripts/EnemyShip.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShip : MonoBehaviour {
+
+	public float health = 150f;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		Projectile missile = col.gameObject.GetComponent<Projectile>();
+
+		if(missile){
+			health -= missile.GetDamage();
+			missile.Hit();
+			if(health <= 0){
+				Destroy(gameObject);
+			}
+		}
+	}
+}
